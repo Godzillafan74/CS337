881f9088eb9229d78b9a912d38497007f3998a5b
Godzillafan74 <55116540+Godzillafan74@users.noreply.github.com>
2021-10-26 15:05:15 -0400

    Add files via upload

---
+++ NJSchoolsHw2/SchoolDataTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SchoolDataTableViewCell.swift
+//  NJSchools
+//
+//  Created by Elias Csipkay on 10/9/21
+//
+
+import UIKit
+
+class SchoolDataTableViewCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
